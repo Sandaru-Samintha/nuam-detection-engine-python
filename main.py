cfef695a3338014f979a98208abf681f7a1bdf3e
Naveendanj <naveenhettiwaththa@gmail.com>
2026-01-16 21:55:37 +0530

    feat: added testing code

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-from engine.core import DetectionEngine
-from utils.packet_source import start_sniffing
-from engine.config import ENABLED_DETECTORS , BACKEND_BASE_URL , BACKEND_PORT
-from logger.logger import Logger
+# from engine.core import DetectionEngine
+# from utils.packet_source import start_sniffing
+# from engine.config import ENABLED_DETECTORS , BACKEND_BASE_URL , BACKEND_PORT
+# from logger.logger import Logger
 
-engine = DetectionEngine(ENABLED_DETECTORS)
+# engine = DetectionEngine(ENABLED_DETECTORS)
 
-# logger = Logger()
-# logger.init_socket_connection(BACKEND_BASE_URL, BACKEND_PORT)
+# # logger = Logger()
+# # logger.init_socket_connection(BACKEND_BASE_URL, BACKEND_PORT)
 
-def on_packet(pkt):
-    packet_type = engine.observe_type(pkt)
-    observed_details = engine.extract_device_info(pkt , packet_type)
-    is_new = engine.is_new_device_joined(observed_details)
+# def on_packet(pkt):
+#     packet_type = engine.observe_type(pkt)
+#     observed_details = engine.extract_device_info(pkt , packet_type)
+#     is_new = engine.is_new_device_joined(observed_details)
     
-    if is_new:
-        event = engine.generate_event(observed_details , detector_name=packet_type + " Detector")
-        # logger.send_event(event)
-        print("New device joined:", event)
+#     if is_new:
+#         event = engine.generate_event(observed_details , detector_name=packet_type + " Detector")
+#         # logger.send_event(event)
+#         print("New device joined:", event)
 
-start_sniffing(on_packet)
+# start_sniffing(on_packet)
+
+from multiprocessing import Process
+from network.runner import generate_test_traffic, start_detection_engine
+
+if __name__ == "__main__":
+    detector_process = Process(target=start_detection_engine)
+    traffic_process = Process(target=generate_test_traffic)
+
+    detector_process.start()
+    traffic_process.start()
+
+    detector_process.join()
+    traffic_process.join()
---
+++ network/runner.py
@@ -0,0 +1,36 @@
+
+import os
+import time
+
+from engine.config import ENABLED_DETECTORS
+from engine.core import DetectionEngine
+from utils.packet_source import start_sniffing
+
+
+def generate_test_traffic():
+    
+    TARGET_IP = "10.0.0.2"
+
+    while True:
+        os.system(f"ping -c 1 {TARGET_IP} > /dev/null 2>&1")
+        os.system(f"arping -c 1 {TARGET_IP} > /dev/null 2>&1")
+        time.sleep(2)
+
+
+
+def start_detection_engine():
+    engine = DetectionEngine(ENABLED_DETECTORS)
+
+    def on_packet(pkt):
+        packet_type = engine.observe_type(pkt)
+        observed_details = engine.extract_device_info(pkt, packet_type)
+        is_new = engine.is_new_device_joined(observed_details)
+
+        if is_new:
+            event = engine.generate_event(
+                observed_details,
+                detector_name=f"{packet_type} Detector"
+            )
+            print("New device joined:", event)
+
+    start_sniffing(on_packet)
